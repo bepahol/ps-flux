4402bcd339eca4c2d63f80d3e9eba2e4de3c9f4b
Benjamin Holst <bepahol2@gmail.com>
2020-03-19 10:23:45 -0600

    module 9 - forms

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>About</h2>
+        <p>This App uses React</p>
+        <a href="/home">Home</a>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
